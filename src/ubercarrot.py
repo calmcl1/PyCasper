a160accebf18d633691d2c3bc746f8c9d521ad8f
Callum McLean <calmcl1@aol.com>
2015-11-27 14:36:45 +0000

    Rewrote basic ui with AUI

--- src/ubercarrot.py
+++ src/ubercarrot.py
@@ -1,4 +1,5 @@
 import wx
+import wx.lib.agw.aui as aui
 
 
 __author__ = 'Callum McLean'
@@ -8,13 +9,19 @@ class MainWindow(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, title="UberCarrot", size=(-1, -1))
 
+        self._mgr = aui.AuiManager()
+
+        # notify AUI which frame to use
+        self._mgr.SetManagedWindow(self)
+
         print "Loading UI..."
         # To avoid having the ugly dark-grey background on Windows, we'll use a wxPanel as the only child object
         # of the wxFrame. All the control will be children of the wxPanel.
-        self.main_panel = wx.Panel(self, -1)
 
-        sizer_frame = wx.BoxSizer(wx.HORIZONTAL)
-        sizer_text_boxes = wx.BoxSizer(wx.VERTICAL) # All the text-stuff and the submit button goes in here.
+        self.panel_text_boxes = wx.Panel(self, -1)
+        sizer_data_entry = wx.BoxSizer(wx.VERTICAL) # All the text-stuff and the submit button goes in here.
+        self.panel_cg_recall = wx.Panel(self, -1)
+        sizer_cg_recall = wx.BoxSizer(wx.VERTICAL) # We'll put all the 'Recall Template' buttons in here
 
         # We need lots of text fields, to enter text in!
         # These also need labels.
@@ -27,32 +34,43 @@ class MainWindow(wx.Frame):
         self.textInputFields = []
 
         for i in xrange(0, num_text_fields):
-            text_field = wx.TextCtrl(self.main_panel)
-            text_field_label = wx.StaticText(self.main_panel, -1, "f{0} text".format(i))
+            text_field = wx.TextCtrl(self.panel_text_boxes)
+            text_field_label = wx.StaticText(self.panel_text_boxes, -1, "f{0} text".format(i))
             print "\tAdding text field: f{0}".format(i)
             controls = [text_field_label, text_field]
             self.textInputFields.append(controls)
             sizer_text_fields.AddMany(controls)
 
         print "\tAdding SUBMIT button" # The important one
-        self.button_submit = wx.Button(self.main_panel, -1, "SUBMIT", style=wx.BU_EXACTFIT)
+        self.button_submit = wx.Button(self.panel_text_boxes, -1, "SUBMIT", style=wx.BU_EXACTFIT)
 
-        sizer_cg_recall = wx.BoxSizer(wx.VERTICAL) # We'll put all the 'Recall Template' buttons in here
+        ################################
+        # RECALL BUTTONS
+        ################################
 
         num_templates = 5 # How many templates shall we have?
         self.template_buttons = []
         for i in xrange(0, num_templates):
             print "\tAdding recall button: #{0}".format(i)
-            btn = wx.Button(self.main_panel, label="Recall Template {0}".format(i))
+            btn = wx.Button(self.panel_cg_recall, label="Recall Template {0}".format(i))
             self.template_buttons.append(btn)
             sizer_cg_recall.Add(btn)
 
-        # All together, now!
-        sizer_text_boxes.Add(sizer_text_fields)
-        sizer_text_boxes.Add(self.button_submit)
-        sizer_frame.AddMany([sizer_text_boxes, sizer_cg_recall])
-        self.main_panel.SetSizer(sizer_frame)
+        ################################
+        # BUILD UI
+        ################################
 
+        # All together, now!
+        sizer_data_entry.Add(sizer_text_fields)
+        sizer_data_entry.Add(self.button_submit)
+        self.panel_text_boxes.SetSizerAndFit(sizer_data_entry)
+
+        self.panel_cg_recall.SetSizerAndFit(sizer_cg_recall)
+
+        self._mgr.AddPane(self.panel_text_boxes, aui.AuiPaneInfo().Left().Caption("Pane Number One"))
+        self._mgr.AddPane(self.panel_cg_recall, aui.AuiPaneInfo().Right().Caption("Pane Number Two"))
+
+        self._mgr.Update()
         # Boom.
         self.Show()
 
