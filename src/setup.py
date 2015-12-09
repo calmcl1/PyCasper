120b7849a05299b338760bf9238ee3845c374ee5
Callum McLean <calmcl1@aol.com>
2015-12-09 22:42:58 +0000

    Added requirements.readthedocs.txt

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='UberCarrot',
+    version='0.1a1',
+    packages=['caspartalk'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Callum McLean',
+    author_email='calmcl1@aol.com',
+    description=''
+)
